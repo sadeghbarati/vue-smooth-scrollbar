818525d8110cd4b96e1420ebd4115e105cb77298
Sadegh Barati <sadeghbaratiwork@gmail.com>
2022-06-13 06:07:16 -0700

    feat: yorkie

--- src/app.vue
+++ src/app.vue
@@ -1,15 +1,7 @@
+<script lang="ts" setup>
+import HelloWorld from './helloWorld.vue'
+</script>
+
 <template>
   <HelloWorld />
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './helloWorld.vue'
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-})
-</script>
