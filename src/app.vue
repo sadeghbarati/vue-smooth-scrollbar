cc571e1c83fbf7ee9490ce7bc4f067d23fbe756d
Sadegh Barati <sadeghbaratiwork@gmail.com>
2022-06-13 13:03:22 -0700

    feat: composition smooth-scroll

--- src/app.vue
+++ src/app.vue
@@ -1,24 +1,83 @@
 <script lang="ts" setup>
-import { useSmoothScrollbar } from '.'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll'
+import { onMounted, ref } from 'vue'
+import ScrollTriggerPlugin from './scrolltrigger'
 
-const [container, scrollbar] = useSmoothScrollbar()
+import { type ScrollbarOptions, useSmoothScrollbar } from '.'
 
-// eslint-disable-next-line no-console
-console.log(scrollbar)
+gsap.registerPlugin(ScrollTrigger)
+
+// const optionsNew = ref<Partial<ScrollbarOptions>>({
+//   alwaysShowTracks: true,
+//   damping: 0.3,
+// })
+const circle = ref()
+
+const [container, scrollbar] = useSmoothScrollbar({
+  alwaysShowTracks: false,
+  damping: 0.1,
+}, [OverscrollPlugin])
+
+onMounted(() => {
+  ScrollTrigger.scrollerProxy('.container', {
+    scrollTop(value) {
+      if (arguments.length)
+        // @ts-expect-error aaa
+        scrollbar.value.scrollTop = value
+
+      return scrollbar?.value?.scrollTop
+    },
+  })
+
+  scrollbar?.value?.addListener(ScrollTrigger.update)
+
+  /* eslint-disable no-console */
+  ScrollTrigger.defaults({
+    scroller: container.value,
+    pinType: 'transform',
+  })
+  console.log(circle.value)
+
+  gsap.to(circle.value, {
+    scrollTrigger: {
+      trigger: circle.value,
+      start: 'top top',
+      end: () => `+=${innerHeight}`,
+      pin: true,
+      scrub: true,
+      markers: true,
+    },
+  })
+
+  gsap.to(circle.value, {
+    x: 300,
+  })
+
+  if (document.querySelector('.gsap-marker-scroller-start')) {
+    const markers = gsap.utils.toArray('[class *= "gsap-marker"]')
+
+    scrollbar?.value?.addListener(({ offset }) => {
+      gsap.set(markers, { marginTop: -offset.y })
+    })
+  }
+})
 </script>
 
 <template>
-  <div ref="container">
+  <div ref="container" class="container">
     <div class="min-h">
-      asdasdasd
+      <div ref="circle" class="circle" />
     </div>
   </div>
 </template>
 
 <style>
-  body {
+  html, body, #app {
     margin: 0;
     padding: 0;
+    width: 100%;
     height: 100%;
     position: relative;
     overflow: hidden;
@@ -30,5 +89,14 @@ console.log(scrollbar)
 
   .min-h {
     min-height: 300vh;
+  }
+
+  .circle {
+    margin: 100px;
+    text-align: center;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    background-color: red;
   }
 </style>
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,43 @@
-import { type Ref, isRef, onMounted, onUnmounted, ref, watch } from 'vue-demi'
+import { type Ref, isRef, onMounted, onUnmounted, ref } from 'vue-demi'
 
+import type Scrollbar from 'smooth-scrollbar'
+import type { ScrollbarPlugin } from 'smooth-scrollbar/plugin'
 import SmoothScrollbar from 'smooth-scrollbar'
-import type { Scrollbar, ScrollbarOptions } from 'smooth-scrollbar/interfaces'
 
-type OptionalScrollOptions = Partial<ScrollbarOptions>
+export interface ScrollbarOptions {
+  /** Momentum reduction damping factor, a float value between `(0, 1)`. The lower the value is, the more smooth the scrolling will be (also the more paint frames). */
+  damping: number
+  /** Minimal size for scrollbar thumbs. */
+  thumbMinSize: number
+  /**
+   * Render every frame in integer pixel values, set to `true` to **improve** scrolling performance.
+   */
+  renderByPixels: boolean
+  /**
+   * Keep scrollbar tracks visible.
+   */
+  alwaysShowTracks: boolean
+  /**
+   * Set to `true` to allow outer scrollbars continue scrolling when current scrollbar reaches edge.
+   */
+  continuousScrolling: boolean
+  /** Delegate wheel events and touch events to the given element. By default, the container element is used. This option will be useful for dealing with fixed elements. */
+  delegateTo: EventTarget | null
+  /** @deprecated `wheelEventTarget` is deprecated and will be removed in the future, use `delegateTo` instead. */
+  wheelEventTarget: EventTarget | null
+  /**
+   * Options for plugins, see {@link https://github.com/idiotWu/smooth-scrollbar/blob/develop/docs/plugin.md Plugin System}.
+   *
+   * */
+  plugins: any
+}
 
-export function useSmoothScrollbar<
-  T extends HTMLElement,
->(options?: Ref<OptionalScrollOptions> | OptionalScrollOptions | undefined, plugins?: any[], gsap?: boolean) {
-  const container = ref<T>()
+export function useSmoothScrollbar(options: Ref<Partial<ScrollbarOptions>> | Partial<ScrollbarOptions> | undefined, plugins?: (typeof ScrollbarPlugin)[]): [Ref<HTMLElement | undefined>, Ref<Scrollbar | undefined>] {
+  const container = ref<HTMLElement>()
   const scrollbar = ref<Scrollbar>()
 
-  // if (isRef(options)) {
-  //   watch(options, (newOptions, _) => {
-  //     if (scrollbar.value)
-
-  //   })
-  // }
+  if (plugins?.length !== 0)
+    plugins?.map(plugin => SmoothScrollbar.use(plugin))
 
   onMounted(() => {
     if (container.value)
